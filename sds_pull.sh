d94ee9df7806137c4482cd3e704f094a947dc830
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-08-28 08:18:51 -0500

    single user pull

--- sds_pull.sh
+++ sds_pull.sh
@@ -5,11 +5,22 @@
 ####
 ################################################################
 
+# optional argument: single user to update
+if [ $# -eq 1 ] ; then
+    targetdir=$1
+fi
+
 for d in * ; do 
-    if [ -d "$d" -a -d "$d/.git" ] ; then 
-	echo "$d"
-	( cd "$d"
-	    git pull
-	)
+    # if no specific target, or this is the specific target
+    if [ -z "$targetdir" -o "$d" = "$targetdir" ] ; then
+	if [ -d "$d" -a -d "$d/.git" ] ; then 
+	    echo "$d"
+	    ( cd "$d"
+	      git pull >../gitmsg 2>&1
+	      if [ $( cat ../gitmsg | grep "no such ref" | wc -l ) -gt 0 ] ; then
+		  echo " .. main branch problem in $d"
+	      fi
+	    )
+	fi
     fi
 done
