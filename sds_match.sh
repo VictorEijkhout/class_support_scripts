490ca592599c8ef1cc2ff6abf99d1554a23bfc98
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-09-05 14:52:16 -0500

    extract-d no longer called project

--- sds_match.sh
+++ sds_match.sh
@@ -10,7 +10,7 @@ if [ $# -ne 3 ] ; then
     echo "Usage: $0 incol invval outcol" && exit 0
 fi
 incol=$1
-inval=$2
+inval=$( echo $2 | tr A-Z a-z )
 outcol=$3
 
 infofile="AllInfo.txt"
@@ -21,12 +21,20 @@ fi
 cat $infofile \
     | tr A-Z a-z \
     | tr -d ' ' \
+    | tr , ' ' \
+    | grep $inval \
+    | awk '{printf("3%s3 6%s6\n",$3,$6)}' \
+    | cat
+
+echo \
+    | awk -v outcol=$outcol -v incol=$incol '{print "3" $3 "3 6" $6 "6"} \
+        {print $(incol) " " $(outcol) }' \
     | cut -d ',' -f $incol -f $outcol \
-    | awk -F ',' -v incol=$incol -v outcol=$outcol \
-	  '{ if (incol>outcol) print $2 FS $1; else print $1 FS $2 }' \
+    | awk -F "," -v incol=$incol -v outcol=$outcol \
+	  '{ if (incol>outcol) print $2 " " $1; else print $1 " " $2 }' \
     | awk -F ',' -v inval=$inval \
 	  '\
 	  $1~inval { print $2 }\
-	  '
+	  ' >/dev/null
 
 
