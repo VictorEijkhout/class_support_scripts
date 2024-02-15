fd0583647fad1262e4ceb504591ed99e22f240f0
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-02-15 15:01:56 -0600

    actually building

--- sds_build.sh
+++ sds_build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ################################################################
 ####
-#### Extract homework submissions.
-#### Usage: check_hw.sh name
+#### Build homework submissions.
+#### Usage: sds_build.sh name
 #### 
 #### This
 #### 1. creates a directory `name'
@@ -14,22 +14,17 @@
 ################################################################
 
 function usage {
-    echo "Usage: $0 [ -h ] [ -a altname ] [ -d ] [ -x ] [ -u user ] homeworkname"
-    echo "    -a : alternative homework name"
-    echo "    -d : find directory by that name and copy as directory"
-    exit 0
+    echo "Usage: $0 [ -u username ] homeworkname"
 }
 
 if [ $# -lt 1 -o "$1" = "-h" ] ; then
     usage
 fi
 
-altname=
-x=
-users=$( ls )
+users=
 while [ $# -gt 1 ] ; do
     if [ "$1" = "-h" ] ; then
-	usage
+	usage && exit 0
     elif [ "$1" = "-a" ] ; then
 	shift && altname=$1 && shift
     elif [ "$1" = "-x" ] ; then
@@ -41,61 +36,41 @@ while [ $# -gt 1 ] ; do
     fi
 done
 if [ $# -eq 0 ] ; then
-    usage
+    usage && exit 1
 fi
 HW=$1
-echo "Extracting: ${HW}" && echo "in users: $users" && echo
-
-mkdir -p $HW
-hwdir=`pwd`/$HW
-
+if [ ! -d "${HW}" ] ; then
+    echo "ERROR can not find homework directory: <<$HW>>" && usage && exit 2
+else
+    cd $HW
+fi
+if [ -z "$users" ] ; then
+    users=$( ls )
+fi
+echo "Building $HW for users: $users"
 for u in $users ; do 
-    if [ -d "$u" ] ; then 
-	if [ ! -z "$x" ] ; then echo && echo "Testing user $u"; fi
-	pushd "$u" >/dev/null
-	found=0
-	## go through all the directories of this students
-	for dd in * ; do 
-	    if [ -d "$dd" ] ; then 
-		## see if it matches (zsh test) the homework
-		if [ ! -z "$x" ] ; then echo && echo " .. testing dir $dd"; fi
-		stdname=$( echo "$dd" | tr A-Z a-z | tr -d "_ " )
-		if [ "$stdname" == *${HW}* -o "$stdname" == "$altname" ] ; then 
-		    found=1
-		    if [ ! -z "$x" ] ; then echo " -- Found dir <<$u/$dd>>" ; fi 
-		    if [ ! -z "${dir}" ] ; then
-			src="${dd}"
-			rm -rf "$hwdir/${u}_dir/$src"
-			cp -r "$src" "$hwdir/${u}_dir"
-			if [ ! -z "$x" ] ; then
-			    echo " .. copy <<$src>> to <<$hwdir/${u}_dir>>"
-			fi
-		    else
-			for src in "$dd"/*.pdf "$dd"/*.cxx "$dd"/*.cpp ; do 
-			    if [ ! -z "$x" ] ; then echo " .. test file $src" ; fi
-			    if [ -f "$src" ] ; then 
-				if [ ! -z "$x" ] ; then echo " .. found file $src" ; fi
-				n=$( echo $src | cut -d '.' -f 1 )
-				e=$( echo $src | cut -d '.' -f 2 )
-				tgt=$u.$e
-				cp "$src" "$hwdir/$tgt" 
-			    fi
-			done 2>/dev/null
-		    fi
-		fi
-	    fi
-	done
-	if [ $found -eq 0 ] ; then
-	    if [ ! -z "$altname" ] ; then 
-		echo " .. $u : no homework ${HW} or ${altname} found"
-	    else
-		echo " .. $u : no homework ${HW} found"
-	    fi
-	    unotfound="$unotfound $u"
+    if [ -d "$u" ] ; then
+	user=${u%%_dir}
+	srcdir=
+	if [ -f $u/CMakeLists.txt ] ; then
+	    srcdir=$u
+	elif [ -f $u/src/CMakeLists.txt ] ; then
+	    srcdir=$u/src
 	fi
-	popd >/dev/null
+	if [ -z "$srcdir" ] ; then
+	    echo "WARNING can not find CMakeLists.txt for user <<$u>>"
+	else
+	    ( rm -rf build \
+		  && mkdir build \
+		  && cd build \
+		  && cmake ../${srcdir} \
+		  && make \
+		) 2>&1 | tee ${user}.log
+	fi
+    else
+	echo "WARNING unknown user: <<$u>>"
     fi
-done | tee extract.log
+done | tee build.log
 echo "see extract.log"
 if [ ! -z "$unotfound" ] ; then
     echo "Homework not found for: $unotfound"
