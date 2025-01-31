72e5471c3b28a9c1b59aac01c3dbd3ff55f211d8
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-01-30 20:58:00 -0600

    readme matching

--- sds_readme.sh
+++ sds_readme.sh
@@ -5,15 +5,22 @@ import re
 import sys
 import subprocess
 
+sys.argv = sys.argv[1:]
 users = None
-if len(sys.argv)>1:
-    if sys.argv[1]=="-h":
+match = None
+while len(sys.argv)>0:
+    if sys.argv[0]=="-h":
         print( f"Usage: {sys.argv[0]} [ -h ] [ -u user ]" )
         sys.exit(0)
-    elif sys.argv[1]=="-u":
-        users = sys.argv[2]
-        sys.argv = sys.argv[2:]
-
+    elif sys.argv[0]=="-u":
+        users = sys.argv[1]
+        sys.argv = sys.argv[1:]
+    elif len(sys.argv)==1 and not re.match(r'\-',sys.argv[0]):
+        match = sys.argv[0]
+        #print( f"matching {match}" )
+        break
+    else:
+        raise f"Unrecognized option or parameter <<{sys.argv[0]}>>"
 
 if not users:
     command = "sds_users.sh"
@@ -23,15 +30,14 @@ if not users:
 
 userdict = {}
 for u in users.split():
-    #print(u)
     userdict[u] = {}
+    matched = False
     with open( f"{u}/README.md" ) as readme:
         for line in readme:
             if not re.search(":",line): continue
             line = line.strip('\n').strip(r"\\").strip("<br/>")
             k,v = line.split(":",1); k = k.lower()
             v = v.lstrip(' ').rstrip(' ').lstrip('*').rstrip('*')
-            #print( f"{line} = {k} : {v}" )
             if re.search("tacc",k):
                 userdict[u]["taccname"] = v
             elif re.search("name",k):
@@ -40,7 +46,11 @@ for u in users.split():
                 userdict[u]["eid"] = v
             elif re.search("github",k):
                 userdict[u]["repo"] = v
-    #print( userdict[u] )
+            #print( f" match <<{match}>> to <<{v}>>" )
+            if match and re.search( match.lower(),v.lower() ):
+                #print( " .. true " )
+                matched = True
+    if match and not matched: continue
     try :
         realname = userdict[u]['realname']
         lastname = re.search(r'[^ ]+$',realname).group(0)
