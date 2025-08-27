918224092fc7660cbe7f22dc2a90912e073c136a
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2025-08-27 12:47:22 -0500

    readme parsing

--- sds_readme.sh
+++ sds_readme.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- python -*-
 
+import os
 import re
 import sys
 import subprocess
 
+script = sys.argv[0]
 sys.argv = sys.argv[1:]
 users = None
 match = None
 while len(sys.argv)>0:
     if sys.argv[0]=="-h":
-        print( f"Usage: {sys.argv[0]} [ -h ] [ -u user ]" )
+        print( f"Usage: {script} [ -h ] [ -u user ]" )
         sys.exit(0)
     elif sys.argv[0]=="-u":
         users = sys.argv[1]
@@ -32,20 +34,29 @@ userdict = {}
 for u in users.split():
     userdict[u] = {}
     matched = False
-    with open( f"{u}/README.md" ) as readme:
+    readmepath = f"{u}/README.md"
+    if not os.path.exists(readmepath):
+        continue
+    print( f"readme for: {u}" )
+    with open( readmepath ) as readme:
         for line in readme:
             if not re.search(":",line): continue
-            line = line.strip('\n').strip(r"\\").strip("<br/>")
+            line = line.strip('\n').strip(r"\\").strip("<br/>").strip(".")
+            #print(line)
             k,v = line.split(":",1); k = k.lower()
             v = v.lstrip(' ').rstrip(' ').lstrip('*').rstrip('*')
             if re.search("tacc",k):
+                #print( f">> tacc: {v}" )
                 userdict[u]["taccname"] = v
-            elif re.search("name",k):
-                userdict[u]["realname"] = v
             elif re.search("eid",k):
+                #print( f">> utid: {v}" )
                 userdict[u]["eid"] = v
             elif re.search("github",k):
+                #print( f">> repo: {v}" )
                 userdict[u]["repo"] = v
+            elif re.search("name",k):
+                #print( f">> name: {v}" )
+                userdict[u]["realname"] = v
             #print( f" match <<{match}>> to <<{v}>>" )
             if match and re.search( match.lower(),v.lower() ):
                 #print( " .. true " )
